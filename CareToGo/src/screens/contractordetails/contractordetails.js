1b1dfb2d54fc2c32e4a2c82a45b4a90ca9091658
leebueyo <67992712+leebueyo@users.noreply.github.com>
2022-11-19 10:28:14 -0500

    Update contractordetails.js

--- CareToGo/src/screens/contractordetails/contractordetails.js
+++ CareToGo/src/screens/contractordetails/contractordetails.js
@@ -5,18 +5,19 @@ import {
   Image,
   TouchableOpacity,
   Dimensions,
+  FlatList,
 } from "react-native";
+import { useState } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, Entypo, FontAwesome } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
-import { Entypo } from "@expo/vector-icons";
-import { Ionicons } from "@expo/vector-icons";
+import tw from "tailwind-react-native-classnames";
 import {
   SharedElement,
   SharedElementTransition,
   nodeFromRef,
 } from "react-native-shared-element";
+import { set } from "react-native-reanimated";
 
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
@@ -24,13 +25,290 @@ const height = Dimensions.get("window").height;
 const ContractorDetails = () => {
   const navigation = useNavigation();
   const route = useRoute();
-
-  const pressHandler = () => {
-    navigation.navigate("orders", {
-      id: route.params.id,
-    });
+  const [stage, setStage] = useState("Contact");
+  const [selected, setSelected] = useState([]);
+  const pressNurse = () => {
+    setStage("Nurse");
+  };
+  const pressContact = () => {
+    setStage("Contact");
+  };
+  const pressPsw = () => {
+    setStage("Psw");
   };
 
+  const Split = () => {
+    if (stage == "Contact") {
+      return (
+        <View style={styles.bg}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <TouchableOpacity
+                onPress={pressContact}
+                style={{
+                  backgroundColor: "#A6C4DD",
+                  height: height * 0.06911,
+                  width: width * 0.14953271,
+                  borderRadius: 32,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <MaterialIcons name="contacts" size={24} color="white" />
+              </TouchableOpacity>
+              <Entypo name="dot-single" size={24} color="#A6C4DD" />
+            </View>
+            <View style={{}}>
+              <TouchableOpacity
+                onPress={pressPsw}
+                style={{
+                  backgroundColor: "#A6C4DD",
+                  height: height * 0.06911,
+                  width: width * 0.14953271,
+                  borderRadius: 32,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  borderColor: "black",
+                }}
+              >
+                <MaterialIcons name="work" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+            <View style={{}}>
+              <TouchableOpacity
+                onPress={pressNurse}
+                style={{
+                  backgroundColor: "#A6C4DD",
+                  height: height * 0.06911,
+                  width: width * 0.14953271,
+                  borderRadius: 32,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <MaterialIcons
+                  name="medical-services"
+                  size={24}
+                  color="white"
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      );
+    }
+    if (stage == "Nurse") {
+      return (
+        <View style={styles.bg}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <View>
+              <TouchableOpacity
+                onPress={pressContact}
+                style={{
+                  backgroundColor: "#A6C4DD",
+                  height: height * 0.06911,
+                  width: width * 0.14953271,
+                  borderRadius: 32,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <MaterialIcons name="contacts" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+            <View style={{}}>
+              <TouchableOpacity
+                onPress={pressPsw}
+                style={{
+                  backgroundColor: "#A6C4DD",
+                  height: height * 0.06911,
+                  width: width * 0.14953271,
+                  borderRadius: 32,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  borderColor: "black",
+                }}
+              >
+                <MaterialIcons name="work" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <TouchableOpacity
+                onPress={pressNurse}
+                style={{
+                  backgroundColor: "#A6C4DD",
+                  height: height * 0.06911,
+                  width: width * 0.14953271,
+                  borderRadius: 32,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <MaterialIcons
+                  name="medical-services"
+                  size={24}
+                  color="white"
+                />
+              </TouchableOpacity>
+              <Entypo name="dot-single" size={24} color="#A6C4DD" />
+            </View>
+          </View>
+          <View style={{ flex: 1 }}>
+            <FlatList
+              data={JSON.parse(route.params.nursingServices)}
+              keyExtractor={(item) => item.id}
+              ItemSeparatorComponent={() => (
+                <View style={tw` border-t border-gray-200 flex-shrink py-0`} />
+              )}
+              renderItem={({
+                item: { id, name, description, price },
+                item,
+              }) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    let newSelected = { ...selected };
+                    newSelected[item.id] = !newSelected[item.id];
+                    setSelected(newSelected);
+                  }}
+                  style={tw`flex-row items-center justify-evenly p-3 ${
+                    selected[id] && "bg-gray-200"
+                  }`}
+                >
+                  <View style={tw`px-3 w-5/6`}>
+                    <Text style={tw`font-semibold text-lg`}>{name}</Text>
+                    <Text style={tw`text-gray-500 `}>{description}</Text>
+                  </View>
+                  <Text style={tw`text-lg `}>${price}</Text>
+                </TouchableOpacity>
+              )}
+            />
+          </View>
+        </View>
+      );
+    }
+    if (stage == "Psw") {
+      return (
+        <View style={styles.bg}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <View>
+              <TouchableOpacity
+                onPress={pressContact}
+                style={{
+                  backgroundColor: "#A6C4DD",
+                  height: height * 0.06911,
+                  width: width * 0.14953271,
+                  borderRadius: 32,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <MaterialIcons name="contacts" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <TouchableOpacity
+                onPress={pressPsw}
+                style={{
+                  backgroundColor: "#A6C4DD",
+                  height: height * 0.06911,
+                  width: width * 0.14953271,
+                  borderRadius: 32,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  borderColor: "black",
+                }}
+              >
+                <MaterialIcons name="work" size={24} color="white" />
+              </TouchableOpacity>
+              <Entypo name="dot-single" size={24} color="#A6C4DD" />
+            </View>
+            <View style={{}}>
+              <TouchableOpacity
+                onPress={pressNurse}
+                style={{
+                  backgroundColor: "#A6C4DD",
+                  height: height * 0.06911,
+                  width: width * 0.14953271,
+                  borderRadius: 32,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <MaterialIcons
+                  name="medical-services"
+                  size={24}
+                  color="white"
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={{ flex: 1 }}>
+            <FlatList
+              data={JSON.parse(route.params.pswServices)}
+              keyExtractor={(item) => item.id}
+              ItemSeparatorComponent={() => (
+                <View style={tw` border-t border-gray-200 flex-shrink py-0`} />
+              )}
+              renderItem={({
+                item: { id, name, description, price },
+                item,
+              }) => (
+                <TouchableOpacity
+                  onPress={() => {
+                    let newSelected = { ...selected };
+                    newSelected[item.id] = !newSelected[item.id];
+                    setSelected(newSelected);
+                  }}
+                  style={tw`flex-row items-center justify-evenly p-3 ${
+                    selected[id] && "bg-gray-200"
+                  }`}
+                >
+                  <View style={tw`px-3 w-5/6`}>
+                    <Text style={tw`font-semibold text-lg`}>{name}</Text>
+                    <Text style={tw`text-gray-500 `}>{description}</Text>
+                  </View>
+                  <Text style={tw`text-lg `}>${price}</Text>
+                </TouchableOpacity>
+              )}
+            />
+          </View>
+        </View>
+      );
+    }
+  };
   return (
     <View style={{ flex: 1 }}>
       <AntDesign
@@ -74,57 +352,7 @@ const ContractorDetails = () => {
         }}
         style={styles.image}
       />
-
-      <View style={styles.bg}>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              backgroundColor: "#A6C4DD",
-              height: 64,
-              width: 64,
-              borderRadius: 32,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <MaterialIcons name="contacts" size={24} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              backgroundColor: "#A6C4DD",
-              height: 64,
-              width: 64,
-              borderRadius: 32,
-              alignItems: "center",
-              justifyContent: "center",
-              borderColor: "black",
-            }}
-          >
-            <MaterialIcons name="work" size={24} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={pressHandler}
-            style={{
-              backgroundColor: "#A6C4DD",
-              height: 64,
-              width: 64,
-              borderRadius: 32,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <MaterialIcons name="medical-services" size={24} color="white" />
-          </TouchableOpacity>
-        </View>
-        <View>
-          <Entypo name="dot-single" size={24} color="black" />
-        </View>
-      </View>
+      <Split />
     </View>
   );
 };
@@ -137,7 +365,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     transform: [{ translateY: height * 0.3 }],
     borderRadius: 32,
-    padding: 10,
+
     paddingTop: 32 + 10,
   },
   image: {
@@ -160,3 +388,4 @@ const styles = StyleSheet.create({
 });
 
 export default ContractorDetails;
+
